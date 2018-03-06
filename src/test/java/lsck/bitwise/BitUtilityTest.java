670fdeab7071f3a7845c467500baf5182b5cae0a
Brian (Brinsky) <Brinsky@users.noreply.github.com>
2018-03-06 12:44:08 -0500

    Remove unneeded import

--- src/test/java/lsck/bitwise/BitUtilityTest.java
+++ src/test/java/lsck/bitwise/BitUtilityTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
