9bc824eb61baa41c441d8e7d67ea9cf634d21ea9
Brian (Brinsky) <Brinsky@users.noreply.github.com>
2018-03-30 16:59:16 -0400

    Add @Override annotations to AbstractLfsr

--- src/main/java/lsck/lfsr/AbstractLfsr.java
+++ src/main/java/lsck/lfsr/AbstractLfsr.java
@@ -5,42 +5,52 @@ import lsck.bitwise.BitVector;
 /** A base class with common code for implementations of {@link Lfsr}. */
 public abstract class AbstractLfsr implements Lfsr {
   
+  @Override
   public void setFill(int ... bits) {
     setFill(BitVector.fromBits(bits));
   }
 
+  @Override
   public void setFillFromInteger(long fill) {
     setFill(BitVector.fromInteger(getLength(), fill));
   }
-  
+
+  @Override
   public void setFillFromInteger(int fill) {
     setFill(BitVector.fromInteger(getLength(), fill));
   }
-  
+
+  @Override
   public void setFillFromInteger(short fill) {
     setFill(BitVector.fromInteger(getLength(), fill));
   }
-  
+
+  @Override
   public void setFillFromInteger(byte fill) {
     setFill(BitVector.fromInteger(getLength(), fill));
   }
 
+  @Override
   public void setTaps(int ... bits) {
     setTaps(BitVector.fromBits(bits));
   }
-  
+
+  @Override
   public void setTapsFromInteger(long taps) {
     setTaps(BitVector.fromInteger(getLength(), taps));
   }
-  
+
+  @Override
   public void setTapsFromInteger(int taps) {
     setTaps(BitVector.fromInteger(getLength(), taps));
   }
-  
+
+  @Override
   public void setTapsFromInteger(short taps) {
     setTaps(BitVector.fromInteger(getLength(), taps));
   }
-  
+
+  @Override
   public void setTapsFromInteger(byte taps) {
     setTaps(BitVector.fromInteger(getLength(), taps));
   }
