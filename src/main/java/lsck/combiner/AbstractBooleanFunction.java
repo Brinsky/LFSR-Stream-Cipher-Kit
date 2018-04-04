4e64b28d7b3f99366745a4797b6b8b3f42561c9c
Brian (Brinsky) <Brinsky@users.noreply.github.com>
2018-04-04 16:20:48 -0400

    Add variants of BooleanFunction.at for all integer types.

--- src/main/java/lsck/combiner/AbstractBooleanFunction.java
+++ src/main/java/lsck/combiner/AbstractBooleanFunction.java
@@ -1,7 +1,29 @@
 package lsck.combiner;
 
+import lsck.bitwise.BitVector;
+
 public abstract class AbstractBooleanFunction implements BooleanFunction {
 
+  @Override
+  public byte at(long args) {
+    return at(BitVector.fromInteger(getArity(), args));
+  }
+
+  @Override
+  public byte at(int args) {
+    return at(BitVector.fromInteger(getArity(), args));
+  }
+
+  @Override
+  public byte at(short args) {
+    return at(BitVector.fromInteger(getArity(), args));
+  }
+
+  @Override
+  public byte at(byte args) {
+    return at(BitVector.fromInteger(getArity(), args));
+  }
+  
   @Override
   public boolean equals(Object o) {
     if (!(o instanceof BooleanFunction)) {
